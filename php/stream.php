6c6d5db2dd0ace80ef3534be0ee4bcbd74e8e6e4
mrxder <info@matthias-keim.com>
2014-12-17 03:28:57 +0100

    Zeigt Streams an
    Zeigt die Links zu den Streams an und gibt sie an shell.php weiter

---
+++ php/stream.php
@@ -0,0 +1,8 @@
+<ul>
+	<li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fstream.laut.fm%2Fbig-up">Big Up (Reggae)</a></li>
+    <li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fstream.uzic.ch%3A9010%2F">UZIC (Minimal)</a></li>
+    <li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fmp3stream7.apasf.apa.at%3A8000%2F">&Ouml;3 (Pop)</a></li>
+    <li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fmp3stream1.apasf.apa.at%3A8000%2F">FM 4 (Pop)</a></li>
+    <li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fstr2.creacast.com%2Fsudtirol1a">S&uuml;dtirol 1 (Pop)</a></li>
+</ul>
+<a href="shell.php?com1=mpc&com2=stop">Stop</a><br>
