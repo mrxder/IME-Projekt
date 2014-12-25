212a9fce606624c555ab761e08b907de9e7fa3ee
mrxder <info@matthias-keim.com>
2014-12-25 11:23:31 +0100

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>MagicRadio</title>
+<link rel="stylesheet" href="style.css">
+<style type="text/css">
+#control-border {
+	display:none;
+	
+}
+
+#menu {
+	width:100%;
+	text-align:center;
+	margin-top:10%;
+	
+}
+
+#menu img {
+	max-height:100%;
+	max-width:30%;
+}
+</style>
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="ring1">
+    	<div id="ring2">
+        	<div id="ring3">
+            	<div id="menu">
+                	<a href="index_files.php"><img src="img/musikPlayer.png"></a>
+                    <a href="index_ukw.php"><img src="img/ukw.png"></a>
+                    <a href="einstellungen.php"><img src="img/einstellungen.png"></a>
+                </div>
+            	
+                <div id="control-border">
+                	<div id="control-bg">
+                    	<div id="control-container">
+                        	<a href="#"><img src="img/home.png"></a>
+                            <a href="#"><img src="img/zuruck.png"></a>
+                            <a href="#"><img src="img/stop.png"></a>
+                            <a href="#"><img src="img/vor.png"></a>
+                            <a href="#"><img src="img/leiser.png"></a>
+                            <a href="#"><img src="img/lauter.png"></a>
+                        </div><!--control-container-end-->
+                    </div><!--control-bg-end-->
+                </div><!--control-border-end-->
+            </div><!--ring3-end-->
+        </div><!--ring2-end-->
+    </div><!--ring1-end-->
+</div><!--wrapper-end-->
+</body>
+</html>
