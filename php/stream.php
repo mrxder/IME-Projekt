4f6e4a2568dcc4be10d67612126468d3b6832322
mrxder <info@matthias-keim.com>
2014-12-25 11:25:18 +0100

    Update stream.php

--- php/stream.php
+++ php/stream.php
@@ -1,8 +1,59 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>MagicRadio</title>
+<link rel="stylesheet" href="style.css">
+<style type="text/css">
+#control-container img {
+	max-width: 20%;
+}
+
+#list-container ul {
+	list-style-image:url(img/www.png);
+}
+
+</style>
+<!--Für smartphones-->
+<meta name="viewport" content="width=device-width, 
+   initial-scale=1.0, 
+   maximum-scale=1.0, 
+   user-scalable=no">
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="ring1">
+    	<div id="ring2">
+        	<div id="ring3">
+            	<div id="list-container">
+
+            			
 <ul>
-	<li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fstream.laut.fm%2Fbig-up">Big Up (Reggae)</a></li>
-    <li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fstream.uzic.ch%3A9010%2F">UZIC (Minimal)</a></li>
-    <li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fmp3stream7.apasf.apa.at%3A8000%2F">&Ouml;3 (Pop)</a></li>
-    <li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fmp3stream1.apasf.apa.at%3A8000%2F">FM 4 (Pop)</a></li>
-    <li><a href="shell.php?topic=mpcstream&com1=http%3A%2F%2Fstr2.creacast.com%2Fsudtirol1a">S&uuml;dtirol 1 (Pop)</a></li>
+	<?php
+		include 'functions.php';
+  		$titel = getIndex(getStreamPath());
+		foreach($titel AS $xyz) {
+			echo "<li><a href=\"shell.php?topic=mpcstream&com1=".urlencode(getStreamURL((getStreamPath()."/".$xyz)))."\">".getHTMLString($xyz, ".txt")."</a>";
+		}
+		?>
 </ul>
-<a href="shell.php?com1=mpc&com2=stop">Stop</a><br>
+
+
+</div>
+                <div id="control-border">
+                	<div id="control-bg">
+                    	<div id="control-container">
+                        	<a href="index.php"><img src="img/home.png"></a>
+                            <a href="shell.php?com1=mpc&com2=stop"><img src="img/stop.png"></a>
+                            <a href="shell.php?com1=mpc&com2=volume&com3=-8"><img src="img/leiser.png"></a>
+                            <a href="shell.php?com1=mpc&com2=volume&com3=%2B8"><img src="img/lauter.png"></a>
+                        </div><!--control-container-end-->
+                    </div><!--control-bg-end-->
+                </div><!--control-border-end-->
+            </div><!--ring3-end-->
+        </div><!--ring2-end-->
+    </div><!--ring1-end-->
+</div><!--wrapper-end-->
+</body>
+</html>
